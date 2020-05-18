1e41d708cbf1d020fb729751dc77d26bb4407dd9
260450135@qq.com <260450135@qq.com>
2020-05-18 23:55:16 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
 import home from '../views/home'
 
 Vue.use(Router)
@@ -7,8 +8,8 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/index',
-      name: 'home'
+      path: '/home', //匹配规则
+      component: home //路由对应的模板对象
     }
   ]
 })
